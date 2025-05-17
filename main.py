4dedb518f7910e70b249a5fe425bc2a6ae16fa6e
Gargosch <98969@student.vitkovickastredni.cz>
2025-05-17 14:35:10 +0200

    Funkce - Zobrazení grací desky

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ SYMBOL_HRAC_2 = "O"
 def inicializuj_desku():
     #Vytvoří prázdnou hrací desku.
     return [[" " for _ in range(VELIKOST_HRACI_DESKY)] for _ in range(VELIKOST_HRACI_DESKY)]
+
+# Funkce pro zobrazení hrací desky
+def zobraz_desku(deska):
+    # Zobrazí aktuální stav hrací desky.
+    for radek in deska:
+        print("|".join(radek))
+        print("-" * (VELIKOST_HRACI_DESKY * 2 - 1))
